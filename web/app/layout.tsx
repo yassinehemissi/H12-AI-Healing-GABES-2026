2a9118fc1c74c928632e50d40763dfead757ce6a
Mohamed Yassine Hemissi <hemissiyassine@gmail.com>
2026-04-20 07:27:12 +0100

    Pushing build fix

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,20 +1,6 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Manrope, Sora, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const sora = Sora({
-  subsets: ["latin"],
-  variable: "--font-heading",
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 
 export const metadata: Metadata = {
   title: "H12 Hackathon",
@@ -27,17 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={cn(
-        "h-full",
-        "antialiased",
-        inter.variable,
-        sora.variable,
-        geistMono.variable,
-        "font-sans"
-      )}
-    >
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans")}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
